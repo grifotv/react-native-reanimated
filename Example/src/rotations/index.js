79177f15dfe1b9adc2d3ed3e2ee1adc273d4b1b6
Danilo Figueiredo <grifotv@googlemail.com>
2020-05-15 23:15:33 +0100

    Remove Text as it's not used

--- Example/src/rotations/index.js
+++ Example/src/rotations/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 import Animated, { Easing } from 'react-native-reanimated';
 
